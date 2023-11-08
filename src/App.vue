3b28a5247285ae8cc48f3e1ea819e209becdbb81
ozod akromov <akromovasadbek@gmail.com>
2023-11-08 20:17:14 +0500

    08.11.2023

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,6 @@
   <RouterView />
 </template>
 
-<<<<<<< HEAD
-<script setup>
-=======
 <script>
->>>>>>> 993446865a1f7576479183b547627f88bbe529b9
 import { RouterView } from 'vue-router'
 </script>
