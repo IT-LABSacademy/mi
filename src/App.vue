de3e15b025affdd69b5ed548386476959eab999c
Sarvar Mannobov <keynow1290@gmail.com>
2023-11-10 18:33:13 +0500

    feat: added Info Section 10.11.23(18:33)

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-<<<<<<< HEAD
-     <RouterView />
-</template>
-
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
-=======
   <TheNavbar />
+  <InfoView />
   <RouterView />
 </template>
 
 <script>
 import { RouterView } from 'vue-router'
 import TheNavbar from './components/TheNavbar.vue';
+import InfoView from './components/InfoView.vue';
 
 export default {
   components: {
-    TheNavbar
-  }
+    TheNavbar,
+    InfoView
+}
 }
 </script>
->>>>>>> 92007e25f128fdda53fcf2f7cf5afeb42193c28d
