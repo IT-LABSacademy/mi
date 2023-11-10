c3ac0f2225060a8ed8189aa71bc93b637debe5c7
IT Labs <itlabsprogrammingacademy4@gmail.com>
2023-11-10 19:35:13 +0500

    test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
-<<<<<<< HEAD
-     <RouterView />
-</template>
-
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
-=======
   <TheNavbar />
   <RouterView />
 </template>
 
 <script>
 import { RouterView } from 'vue-router'
-import TheNavbar from './components/TheNavbar.vue';
+import TheNavbar from '../components/TheNavbar.vue';
 
 export default {
   components: {
@@ -22,4 +13,3 @@ export default {
   }
 }
 </script>
->>>>>>> 92007e25f128fdda53fcf2f7cf5afeb42193c28d
